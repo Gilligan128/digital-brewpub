04c064c4dd73fae7d08566d4ff331034f66f5e7b
Mark Henke <gilligan128@gmail.com>
2017-06-04 20:37:45 -0400

    changed Google Auth Functional Testto actually test something

---
+++ Digital.BrewPub.Test/BreweryDBClassificationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Digital.BrewPub.Test
+{
+    public class BreweryDBClassificationTest
+    {
+        [Fact]
+        [Trait("Category","Classification")]
+        public void testingExplicitRun()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
--- Digital.BrewPub.Test/Slow/OauthGoogleTest.cs
+++ Digital.BrewPub.Test/Slow/OauthGoogleTest.cs
@@ -32,7 +32,8 @@ namespace Digital.BrewPub.Test.Slow
             var response = await client.PostAsync("/Account/ExternalLogin", content);
             var responseContent = await response.Content.ReadAsStringAsync();
 
-           // response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.StatusCode.Should().Be(HttpStatusCode.Redirect);
+            response.Headers.Location.AbsoluteUri.Contains("https://accounts.google.com/o/oauth2/auth?response_type=code").Should().BeTrue();
 
         }
 
