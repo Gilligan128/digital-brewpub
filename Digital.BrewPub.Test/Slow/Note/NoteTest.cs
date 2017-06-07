1f0c081b28487ac84c8fbd3b22f3b8605b361ee3
Mark Henke <gilligan128@gmail.com>
2017-06-06 23:24:33 -0400

    Added Functional Test for Note-making

--- Digital.BrewPub.Test/Slow/Note/NoteTest.cs
+++ Digital.BrewPub.Test/Slow/Note/NoteTest.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using Xunit;
+using Digital.BrewPub.Features.Note;
+using System.Threading.Tasks;
+using System.Linq;
+using FluentAssertions;
 
 namespace Digital.BrewPub.Test.Slow.Note
 {
     public class NoteTest
     {
         [Fact]
-        public void EnthusiastsMakeNotes()
+        public async Task EnthusiastsMakeNotes()
         {
+            using(var fixture = new FunctionalTestFixture())
+            {
+                IEnumerable<KeyValuePair<string, string>> formValues = new Dictionary<string, string>()
+                {
+                    {nameof(NoteController.MakeInput.Text), "I love it" }
+                };
+                System.Net.Http.HttpContent content = new FormUrlEncodedContent(formValues);
 
+                var response = await fixture.Client.PostAsync("/Note/Make/Love Shack", content);
+
+                var note = fixture.DbContext.Notes.First(n => n.Brewery.Equals("Love Shack"));
+                note.Text.Should().Be("I love it");
+            }
         }
     }
 }
